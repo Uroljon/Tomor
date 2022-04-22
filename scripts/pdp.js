9eef6f96501b035ed337cfc12cab4ebf7705c8ff
Uroljon <uroljonkhidirboev@gmail.com>
2022-04-22 16:01:42 +0500

    pdp is finished

--- scripts/pdp.js
+++ scripts/pdp.js
@@ -1,15 +1,22 @@
 import { navbar } from "./navbar.js"
 
 navbar()
-
+document.querySelector(".information__nav").addEventListener("click", e => {
+    let target = e.target.getAttribute("data-target");
+    document.querySelector(".information__content .active").classList.remove("active");
+    document.querySelector(".information__nav .active").classList.remove("active");
+    e.target.classList.add("active")
+    document.querySelector(`.information__content .${target}`).classList.add("active")
+})
 $('.carousel__wrapper').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    dots: false,
+    dots: true,
     vertical: true,
     verticalSwiping: true,
+    appendDots: ".dots",
     responsive: [
         {
             breakpoint: 992,
@@ -18,14 +25,78 @@ $('.carousel__wrapper').slick({
                 slidesToScroll: 1,
                 vertical: false,
                 verticalSwiping: false,
+                autoplay: true,
                 dots: true,
                 arrows: true,
                 prevArrow: ".carousel__btn--right",
                 nextArrow: ".carousel__btn--left",
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
+    ]
+});
+$('.carousel_2_wrapper').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    prevArrow: ".carousel__btn--ri",
+    nextArrow: ".carousel_2_next",
+    autoplay: true,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 300,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
+$('.carousel_3_wrapper').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    prevArrow: ".carousel__btn--r",
+    nextArrow: ".carousel_3_next",
+    autoplay: true,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 300,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
     ]
 });
